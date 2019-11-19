dcff7e374f620efefb9f2f26930576f235bb5cf5
Richard <richarddewan@hotmail.com>
2019-11-19 14:35:04 +0700

    first commit

---
+++ packagemanagerhelper/src/main/java/com/dewan/packagemanagerhelper/InstalledAppProperty.java
@@ -0,0 +1,106 @@
+package com.dewan.packagemanagerhelper;
+
+import android.graphics.drawable.Drawable;
+
+public class InstalledAppProperty {
+
+    private int id;
+    private String deviceId;
+    private String appName;
+    private String packageName;
+    private Drawable appIcon;
+    private String appIconBase64;
+    private String versionName;
+    private int versionCode;
+    private boolean isSystemApp;
+    private boolean isLocked;
+    private String appExistMsg;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public Drawable getAppIcon() {
+        return appIcon;
+    }
+
+    public void setAppIcon(Drawable appIcon) {
+        this.appIcon = appIcon;
+    }
+
+    public String getAppIconBase64() {
+        return appIconBase64;
+    }
+
+    public void setAppIconBase64(String appIconBase64) {
+        this.appIconBase64 = appIconBase64;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public int getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(int versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public boolean isSystemApp() {
+        return isSystemApp;
+    }
+
+    public void setSystemApp(boolean systemApp) {
+        isSystemApp = systemApp;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void setLocked(boolean locked) {
+        isLocked = locked;
+    }
+
+    public String getAppExistMsg() {
+        return appExistMsg;
+    }
+
+    public void setAppExistMsg(String appExistMsg) {
+        this.appExistMsg = appExistMsg;
+    }
+}
---
+++ packagemanagerhelper/src/main/java/com/dewan/packagemanagerhelper/PackageManagerHelper.java
@@ -0,0 +1,112 @@
+package com.dewan.packagemanagerhelper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PackageManagerHelper {
+    private static final String TAG = "PackageManagerHelper";
+    private Context context;
+    private PackageManager packageManager;
+    private InstalledAppProperty appProperty;
+    private SharedPreferences sharedPreferences;
+    private String deviceId;
+
+    public PackageManagerHelper(Context context, PackageManager packageManager) {
+        this.context = context;
+        this.packageManager = packageManager;
+    }
+
+    public ArrayList<InstalledAppProperty> getInstalledAppInfo() {
+        ArrayList<InstalledAppProperty> appList = new ArrayList<>();
+        packageManager = context.getPackageManager();
+        List<ApplicationInfo> applicationInfo = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo appInfo : applicationInfo) {
+            //check for app with launcher intent
+            if (packageManager.getLaunchIntentForPackage(appInfo.packageName) != null) {
+                appProperty = new InstalledAppProperty();
+                appProperty.setDeviceId(getDeviceId());
+                appProperty.setAppName(String.valueOf(packageManager.getApplicationLabel(appInfo)));
+                appProperty.setPackageName(appInfo.packageName);
+                appProperty.setAppIcon(getAppIconByName(appInfo.packageName));
+                appProperty.setVersionName(getAppVersionName(appInfo.packageName));
+                appProperty.setVersionCode(getAppVersionCode(appInfo.packageName));
+                appProperty.setSystemApp(isSystemApp(appInfo.packageName));
+                appList.add(appProperty);
+            }
+
+        }
+
+        return appList;
+    }
+
+    public Drawable getAppIconByName(String packageName) {
+        Drawable appIcon = null;
+        try {
+            appIcon = packageManager.getApplicationIcon(packageName);
+        } catch (PackageManager.NameNotFoundException er) {
+            Log.e(TAG, er.getMessage());
+        }
+        return appIcon;
+    }
+
+    public String getAppVersionName(String packageName) {
+        String versionName = "";
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_META_DATA);
+            versionName = packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException er) {
+            Log.d(TAG, er.getMessage());
+        }
+
+        return versionName;
+    }
+
+    public int getAppVersionCode(String packageName) {
+        int versionCode = 0;
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_META_DATA);
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                long l = packageInfo.getLongVersionCode();
+                versionCode = (int) l;
+            } else {
+                versionCode = packageInfo.versionCode;
+            }
+        } catch (PackageManager.NameNotFoundException er) {
+            Log.d(TAG, er.getMessage());
+        }
+        return versionCode;
+    }
+
+    public boolean isSystemApp(String packageName) {
+        boolean isTrue = false;
+        try {
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+            if ((applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                if ((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                    isTrue = true;
+                }
+            }
+        } catch (PackageManager.NameNotFoundException er) {
+            Log.d(TAG, er.getMessage());
+        }
+        return isTrue;
+    }
+
+    private String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+}
